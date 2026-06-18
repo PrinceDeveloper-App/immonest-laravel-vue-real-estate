d155e4acd387a0c7e9716617816f332022e1dc7c
Prince Mathew <mathewprincech@gmail.com>
2026-06-18 19:32:58 +0200

    feat(offer): improve offer UI layout and styling

--- resources/js/Pages/Realtor/Show/Components/Offer.vue
+++ resources/js/Pages/Realtor/Show/Components/Offer.vue
@@ -1,7 +1,6 @@
 <template>
   <Box>
-    <template #header>Offer #{{ offer.id }} <span v-if="offer.accepted_at" class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1">accepeted</span></template>
-    
+    <template #header>Offer #{{ offer.id }} <span v-if="offer.accepted_at" class="dark:bg-green-900 dark:text-green-200 bg-green-200 text-green-900 p-1 rounded-md uppercase ml-1">accepted</span></template>    
     <section class="flex items-center justify-between">
       <div>
         <Price :price="offer.amount" class="text-xl" />
@@ -20,7 +19,7 @@
       </div>
       <div>
         <Link
-          v-if="notSold"
+          v-if="!isSold"
           :href="route('realtor.offer.accept', { offer: offer.id })"
           class="btn-outline text-xs font-medium"
           method="put" 
@@ -43,6 +42,7 @@ import { route } from 'ziggy-js'
 const props = defineProps({
   offer: Object,
   listingPrice: Number,
+  isSold: Boolean,
 })
 const difference = computed(
   () => props.offer.amount - props.listingPrice,
@@ -50,7 +50,5 @@ const difference = computed(
 const madeOn = computed(
   () => new Date(props.offer.created_at).toDateString(),
 )
-const notSold = computed(
-  () => !props.offer.accepted_at && !props.offer.rejected_at,
-)
+
 </script>
