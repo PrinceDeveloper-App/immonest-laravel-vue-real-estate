81ec4d0567b39852821611cf2d0adb9a249081f5
Prince Mathew <mathewprincech@gmail.com>
2026-06-19 14:51:25 +0200

    feat(notifications): feat(notifications): add notification icon with unread count badge

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -9,6 +9,13 @@
           <Link :href="route('listing.index')">ImmoNest</Link>
         </div>
         <div v-if="user" class="flex items-center space-x-4">
+          <div class="text-gray-500 relative pr-2 py-2 text-lg">
+            🔔
+            <div class="absolute right-0 top-0 w-5 h-5 bg-red-700 dark:bg-red-400 text-white font-medium border border-white dark:border-gray-900 rounded-full text-xs text-center">
+              {{ notificationCount }}
+            </div>
+          </div>
+
           <Link class="text-sm text-gray-600 dark:text-gray-400" :href="route('realtor.listing.index')">Hello, {{ user.name }}</Link>
           <Link :href="route('realtor.listing.create')" class="btn-primary">+ New Listing</Link>
           <Link :href="route('logout')" class="btn-secondary" method="delete">Logout</Link>
@@ -37,6 +44,9 @@ import { route } from 'ziggy-js'
 const page = usePage()
 const flashSuccess = computed(() => page.props.flash.success)
 const user = computed(() => page.props.user)
+const notificationCount = computed(
+  () => Math.min(page.props.user?.notificationCount ?? 0, 9),
+)
 </script>
 
 <style scoped>
