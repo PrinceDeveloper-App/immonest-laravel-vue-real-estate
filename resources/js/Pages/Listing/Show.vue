7b92dfb25d6bbf47d6a344bd42fbfece4fffff54
Prince Mathew <mathewprincech@gmail.com>
2026-06-19 16:24:49 +0200

    feat(notifications): create empty state component

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
-    <Box class="md:col-span-7 flex items-center">
-      <div v-if="listing.images.length" class="mt-4 grid grid-cols-2 gap-2">
+    <Box v-if="listing.images.length" class="md:col-span-7 flex items-center">
+      <div class="grid grid-cols-2 gap-1">
         <img v-for="image in listing.images" :key="image.id" :src="image.src" />
       </div>
-      <div v-else class="w-full text-center font-medium text-gray-500">No Images</div>
     </Box>
+    <EmptyState v-else class="md:col-span-7 flex items-center">No images</EmptyState>
     <div class="md:col-span-5 flex flex-col gap-4">
       <Box>
         <template #header>
@@ -78,6 +78,7 @@ import {computed, ref} from 'vue'
 import { useMonthlyPayment } from '@/Composables/useMonthlyPayment'
 import { usePage } from '@inertiajs/vue3'
 import OfferMade from './Show/Components/OfferMade.vue'
+import EmptyState from '@/Components/UI/EmptyState.vue'
 
 const interestRate = ref(2.5)
 const duration = ref(25)
