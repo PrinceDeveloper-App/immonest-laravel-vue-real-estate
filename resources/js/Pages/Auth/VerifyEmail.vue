7f42666c7caf437bb5263d60c1f3883ec457b1f7
Prince Mathew <mathewprincech@gmail.com>
2026-06-21 23:14:21 +0200

    feat(email): implement email resend functionality

--- resources/js/Pages/Auth/VerifyEmail.vue
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import { Link } from '@inertiajs/vue3'
+
+</script>
+
 <template>
-  You need to verify to see this page!
+  <div>
+    You need to verify to see this page!
+  </div>
+  <div>
+    <Link 
+      :href="route('verification.send')" 
+      method="post" as="button" class="text-indigo-600 dark:text-indigo-400"
+    >
+      Resend verification Mail
+    </Link>
+  </div>
 </template>
